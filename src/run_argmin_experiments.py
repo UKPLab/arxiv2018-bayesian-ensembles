154421fc5012d3ca0736c0665eb1b89f1d53bd49
Edwin Simpson <simpson@ukp.informatik.tu-darmstadt.de>
2017-12-04 10:23:15 +0100

    current settings for argmin experiments

--- src/run_argmin_experiments.py
+++ src/run_argmin_experiments.py
@@ -15,26 +15,30 @@ gt, annos, doc_start = load_data.load_argmin_data()
 exp.save_results = True
 exp.num_classes = 3
 
-# TODO: address lower bound bug by holding each variable fixed and testing in turn. Then rerun prior_1 and prior_2. 
-
 # priorstr = 'prior_1' # ones along diagonals, alpha0 excludes transitions from outside (1 or -1) to inside (0)
 # exp.bac_alpha0 = np.ones((exp.num_classes, exp.num_classes, exp.num_classes+1, 
 #                           annos.shape[1])) +  1.0 * np.eye(exp.num_classes)[:,:,None,None]
-# exp.exclusions = {}
 
-
-priorstr = 'prior_2' # fives along diagonals, alpha0 excludes transitions from outside (1 or -1) to inside (0)
+# TODO: why is the result different from the prior_2 result if we haven't changed anything? Something must have changed.
+priorstr = 'prior_2_try2' # fives along diagonals, alpha0 excludes transitions from outside (1 or -1) to inside (0)
 exp.bac_alpha0 = np.ones((exp.num_classes, exp.num_classes, exp.num_classes+1, 
                           annos.shape[1])) +  5.0 * np.eye(exp.num_classes)[:,:,None,None]
-exp.exclusions = {}
 
-# priorstr = 'prior_3' # We address the imblance between rows with exclusions and rows without. When there is an exclusion,
-# weights on the correct answer are 0.667 instead of 0.5. Weights on the incorrect answer are 0.333 instead of 0.25, so
-# the correct answer benefits more; should put more weight onto chains of O labels being correct? 
-# TODO: How to address this below? 
+# priorstr = 'prior_3' # We address the imbalance between rows with exclusions and rows without. When there is an exclusion,
+# # weights on the correct answer are 0.667 instead of 0.5. Weights on the incorrect answer are 0.333 instead of 0.25, so
+# # the correct answer benefits more. Solution: put the pseudo-counts from the excluded transition onto the next-best answer,
+# # e.g. the best answer given that the previous one had missed the start of the annotation.
+# # BAC has now been changed to make this work: 
 # exp.bac_alpha0 = np.ones((exp.num_classes, exp.num_classes, exp.num_classes+1, 
-#                           annos.shape[1])) +  1.0 * np.eye(exp.num_classes)[:,:,None,None]
-# exp.exclusions = {}
+#                            annos.shape[1])) +  1.0 * np.eye(exp.num_classes)[:,:,None,None]
+
+# priorstr = 'prior_4' 
+# exp.bac_alpha0 = np.ones((exp.num_classes, exp.num_classes, exp.num_classes+1, 
+#                            annos.shape[1])) +  5.0 * np.eye(exp.num_classes)[:,:,None,None]
+
+# priorstr = 'prior_5' 
+# exp.bac_alpha0 = 0.1 * np.ones((exp.num_classes, exp.num_classes, exp.num_classes+1, 
+#                            annos.shape[1])) +  0.1 * np.eye(exp.num_classes)[:,:,None,None]
 
 results, preds, _ = exp.run_methods(annos, gt, doc_start, -666, './data/argmin/annos.csv')
 
