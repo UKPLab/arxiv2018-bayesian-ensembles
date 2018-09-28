8542e34a9e024c159b3b8cb065da59d2b6af7095
Edwin Simpson <simpson@ukp.informatik.tu-darmstadt.de>
2018-09-28 17:57:13 +0200

    fix for ensuring that the integrated LSTM does not disrupt BSC convergence

--- src/run_ner_experiments_gpu.py
+++ src/run_ner_experiments_gpu.py
@@ -97,9 +97,6 @@ best_bac_wm_score = -np.inf
 #
 # print('best BAC method tested here = %s' % best_bac_wm)
 
-# Niter = 10 !!!!!!!!!!!!!!!!!!!!!!!!!
-niter = 10
-
 # --------------------
 
 exp = Experiment(None, 9, annos.shape[1], None, alpha0_factor=16, alpha0_diags=1, max_iter=niter)
@@ -114,6 +111,7 @@ exp.alpha0_acc_bias = best_acc_bias
 # run all the methods that don't require tuning here
 exp.methods =  [
                 best_bac_wm + '_integrateBOF_integrateLSTM_atEnd',
+                best_bac_wm + '_integrateBOF_integrateLSTM',
 ]
 
 # should run both task 1 and 2.
@@ -124,72 +122,74 @@ exp.run_methods(
     new_data=regen_data
 )
 
-# --------------------
-exp = Experiment(None, 9, annos.shape[1], None, alpha0_factor=16, alpha0_diags=1, max_iter=niter)
-exp.save_results = True
-exp.opt_hyper = False#True
-
-exp.alpha0_diags = best_diags
-exp.alpha0_factor = best_factor
-exp.nu0_factor = best_nu0factor
-exp.alpha0_acc_bias = best_acc_bias
-
-# run all the methods that don't require tuning here
-exp.methods =  [
-                best_bac_wm + '_integrateBOF_then_LSTM',
-]
-
-# should run both task 1 and 2.
-exp.run_methods(
-    annos, gt, doc_start, output_dir, text,
-    ground_truth_val=gt_val, doc_start_val=doc_start_val, text_val=text_val,
-    ground_truth_nocrowd=gt_nocrowd, doc_start_nocrowd=doc_start_nocrowd, text_nocrowd=text_nocrowd,
-    new_data=regen_data
-)
-
-# reset to free memory? ------------------------------------------------------------------------------------------------
-exp = Experiment(None, 9, annos.shape[1], None, alpha0_factor=16, alpha0_diags=1, max_iter=niter)
-exp.save_results = True
-exp.opt_hyper = False#True
-
-exp.alpha0_diags = best_diags
-exp.alpha0_factor = best_factor
-exp.nu0_factor = best_nu0factor
-exp.alpha0_acc_bias = best_acc_bias
-
-# run all the methods that don't require tuning here
-exp.methods =  [
-                'HMM_crowd_then_LSTM',
-]
-
-# should run both task 1 and 2.
-exp.run_methods(
-    annos, gt, doc_start, output_dir, text,
-    ground_truth_val=gt_val, doc_start_val=doc_start_val, text_val=text_val,
-    ground_truth_nocrowd=gt_nocrowd, doc_start_nocrowd=doc_start_nocrowd, text_nocrowd=text_nocrowd,
-    new_data=regen_data
-)
-
-# reset to free memory? ------------------------------------------------------------------------------------------------
-exp = Experiment(None, 9, annos.shape[1], None, alpha0_factor=16, alpha0_diags=1, max_iter=niter)
-exp.save_results = True
-exp.opt_hyper = False#True
-
-exp.alpha0_diags = best_diags
-exp.alpha0_factor = best_factor
-exp.nu0_factor = best_nu0factor
-exp.alpha0_acc_bias = best_acc_bias
-
-#run all the methods that don't require tuning here
-exp.methods =  [
-                'gt_then_LSTM',
-]
-
-# should run both task 1 and 2.
-
-exp.run_methods(
-    annos, gt, doc_start, output_dir, text,
-    ground_truth_val=gt_val, doc_start_val=doc_start_val, text_val=text_val,
-    ground_truth_nocrowd=gt_nocrowd, doc_start_nocrowd=doc_start_nocrowd, text_nocrowd=text_nocrowd,
-    new_data=regen_data, ground_truth_all_points=gt_all
-)
+# # Niter = 10 !!!!!!!!!!!!!!!!!!!!!!!!!
+# niter = 10
+# # --------------------
+# exp = Experiment(None, 9, annos.shape[1], None, alpha0_factor=16, alpha0_diags=1, max_iter=niter)
+# exp.save_results = True
+# exp.opt_hyper = False#True
+#
+# exp.alpha0_diags = best_diags
+# exp.alpha0_factor = best_factor
+# exp.nu0_factor = best_nu0factor
+# exp.alpha0_acc_bias = best_acc_bias
+#
+# # run all the methods that don't require tuning here
+# exp.methods =  [
+#                 best_bac_wm + '_integrateBOF_then_LSTM',
+# ]
+#
+# # should run both task 1 and 2.
+# exp.run_methods(
+#     annos, gt, doc_start, output_dir, text,
+#     ground_truth_val=gt_val, doc_start_val=doc_start_val, text_val=text_val,
+#     ground_truth_nocrowd=gt_nocrowd, doc_start_nocrowd=doc_start_nocrowd, text_nocrowd=text_nocrowd,
+#     new_data=regen_data
+# )
+#
+# # reset to free memory? ------------------------------------------------------------------------------------------------
+# exp = Experiment(None, 9, annos.shape[1], None, alpha0_factor=16, alpha0_diags=1, max_iter=niter)
+# exp.save_results = True
+# exp.opt_hyper = False#True
+#
+# exp.alpha0_diags = best_diags
+# exp.alpha0_factor = best_factor
+# exp.nu0_factor = best_nu0factor
+# exp.alpha0_acc_bias = best_acc_bias
+#
+# # run all the methods that don't require tuning here
+# exp.methods =  [
+#                 'HMM_crowd_then_LSTM',
+# ]
+#
+# # should run both task 1 and 2.
+# exp.run_methods(
+#     annos, gt, doc_start, output_dir, text,
+#     ground_truth_val=gt_val, doc_start_val=doc_start_val, text_val=text_val,
+#     ground_truth_nocrowd=gt_nocrowd, doc_start_nocrowd=doc_start_nocrowd, text_nocrowd=text_nocrowd,
+#     new_data=regen_data
+# )
+#
+# # reset to free memory? ------------------------------------------------------------------------------------------------
+# exp = Experiment(None, 9, annos.shape[1], None, alpha0_factor=16, alpha0_diags=1, max_iter=niter)
+# exp.save_results = True
+# exp.opt_hyper = False#True
+#
+# exp.alpha0_diags = best_diags
+# exp.alpha0_factor = best_factor
+# exp.nu0_factor = best_nu0factor
+# exp.alpha0_acc_bias = best_acc_bias
+#
+# #run all the methods that don't require tuning here
+# exp.methods =  [
+#                 'gt_then_LSTM',
+# ]
+#
+# # should run both task 1 and 2.
+#
+# exp.run_methods(
+#     annos, gt, doc_start, output_dir, text,
+#     ground_truth_val=gt_val, doc_start_val=doc_start_val, text_val=text_val,
+#     ground_truth_nocrowd=gt_nocrowd, doc_start_nocrowd=doc_start_nocrowd, text_nocrowd=text_nocrowd,
+#     new_data=regen_data, ground_truth_all_points=gt_all
+# )
