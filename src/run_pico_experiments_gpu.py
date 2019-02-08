575e95d4a48c00c744965d1b6e9b7cdb41f1cf48
Edwin Simpson <simpson@ukp.informatik.tu-darmstadt.de>
2019-02-08 11:54:21 +0100

    check the PICO BSC+LSTM expt

--- src/run_pico_experiments_gpu.py
+++ src/run_pico_experiments_gpu.py
@@ -34,25 +34,25 @@ diags = [0.1, 1, 10, 100]
 factors = [0.1, 1, 10]
 
 #  ------------------------------------------------------------------------------------------------
-exp = Experiment(None, 3, annos.shape[1], None, max_iter=20)
-
-exp.save_results = True
-exp.opt_hyper = False
-
-exp.alpha0_diags = best_diags
-exp.alpha0_factor = best_factor
-exp.nu0_factor = best_nu0factor
-
-exp.methods =  [
-                best_bac_wm + '_integrateIF_then_LSTM',
-                #'HMM_crowd_then_LSTM',
-]
-
-# this will run task 1 -- train on all crowdsourced data, test on the labelled portion thereof
-exp.run_methods(annos, gt, doc_start, output_dir, text,
-                ground_truth_val=gt_dev, doc_start_val=doc_start_dev, text_val=text_dev,
-                new_data=regen_data
-                )
+# exp = Experiment(None, 3, annos.shape[1], None, max_iter=20)
+#
+# exp.save_results = True
+# exp.opt_hyper = False
+#
+# exp.alpha0_diags = best_diags
+# exp.alpha0_factor = best_factor
+# exp.nu0_factor = best_nu0factor
+#
+# exp.methods =  [
+#                 best_bac_wm + '_integrateIF_then_LSTM',
+#                 #'HMM_crowd_then_LSTM',
+# ]
+#
+# # this will run task 1 -- train on all crowdsourced data, test on the labelled portion thereof
+# exp.run_methods(annos, gt, doc_start, output_dir, text,
+#                 ground_truth_val=gt_dev, doc_start_val=doc_start_dev, text_val=text_dev,
+#                 new_data=regen_data
+#                 )
 
 # reset ------------------------------------------------------------------------------------------------
 exp = Experiment(None, 3, annos.shape[1], None, max_iter=20)
