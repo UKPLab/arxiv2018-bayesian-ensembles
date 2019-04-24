d591089e01b90397743a55af236f60fb7d04dd69
edwinrobots <edwin.simpson@gmail.com>
2019-04-25 00:43:21 +0200

    rerun pico

--- src/run_pico_experiments.py
+++ src/run_pico_experiments.py
@@ -20,8 +20,9 @@ exp = Experiment(None, 3, annos.shape[1], None, max_iter=20)
 exp.save_results = True
 exp.opt_hyper = False #True
 
-exp.alpha0_diags = 100
-exp.alpha0_factor = 9
+exp.nu0_factor = 0.1
+exp.alpha0_diags = 10
+exp.alpha0_factor = 1
 
 best_bac_wm = 'bac_seq' # choose model with best score for the different BAC worker models
 best_nu0factor = 0.1
@@ -45,29 +46,29 @@ methods_to_tune = [
                 # 'bac_seq_integrateIF_noHMM',
                    ]
 
-best_bac_wm_score = -np.inf
-
-# tune with small dataset to save time
-idxs = np.argwhere(gt_task1_dev != -1)[:, 0]
-idxs = np.concatenate((idxs, np.argwhere(gt != -1)[:, 0]))
-idxs = np.concatenate((idxs, np.argwhere((gt == -1) & (gt_task1_dev == -1))[:300, 0]))  # 100 more to make sure the dataset is reasonable size
-
-tune_gt_dev = gt_task1_dev[idxs]
-tune_annos = annos[idxs]
-tune_doc_start = doc_start[idxs]
-tune_text = text[idxs]
-
-for m, method in enumerate(methods_to_tune):
-    print('TUNING %s' % method)
-
-    best_scores = exp.tune_alpha0(diags, factors, nu_factors, method, tune_annos, tune_gt_dev, tune_doc_start,
-                                  output_dir, tune_text, metric_idx_to_optimise=11)
-    best_idxs = best_scores[1:].astype(int)
-    exp.nu0_factor = nu_factors[best_idxs[0]]
-    exp.alpha0_diags = diags[best_idxs[1]]
-    exp.alpha0_factor = factors[best_idxs[2]]
-
-    print('Best values: %f, %f, %f' % (exp.nu0_factor, exp.alpha0_diags, exp.alpha0_factor))
+# best_bac_wm_score = -np.inf
+#
+# # tune with small dataset to save time
+# idxs = np.argwhere(gt_task1_dev != -1)[:, 0]
+# idxs = np.concatenate((idxs, np.argwhere(gt != -1)[:, 0]))
+# idxs = np.concatenate((idxs, np.argwhere((gt == -1) & (gt_task1_dev == -1))[:300, 0]))  # 100 more to make sure the dataset is reasonable size
+#
+# tune_gt_dev = gt_task1_dev[idxs]
+# tune_annos = annos[idxs]
+# tune_doc_start = doc_start[idxs]
+# tune_text = text[idxs]
+#
+# for m, method in enumerate(methods_to_tune):
+#     print('TUNING %s' % method)
+#
+#     best_scores = exp.tune_alpha0(diags, factors, nu_factors, method, tune_annos, tune_gt_dev, tune_doc_start,
+#                                   output_dir, tune_text, metric_idx_to_optimise=11)
+#     best_idxs = best_scores[1:].astype(int)
+#     exp.nu0_factor = nu_factors[best_idxs[0]]
+#     exp.alpha0_diags = diags[best_idxs[1]]
+#     exp.alpha0_factor = factors[best_idxs[2]]
+#
+#     print('Best values: %f, %f, %f' % (exp.nu0_factor, exp.alpha0_diags, exp.alpha0_factor))
 
 #     # this will run task 1 -- train on all crowdsourced data, test on the labelled portion thereof
 #     exp.methods = [method]
@@ -84,31 +85,32 @@ for m, method in enumerate(methods_to_tune):
 # #         best_nu0factor = exp.nu0_factor
 #
 # #  ------------------------------------------------------------------------------------------------
-# exp = Experiment(None, 3, annos.shape[1], None, max_iter=20)
-#
-# exp.save_results = True
-# exp.opt_hyper = False
-#
-# # exp.alpha0_diags = best_diags
-# # exp.alpha0_factor = best_factor
-# # exp.nu0_factor = best_nu0factor
-# #
-# # # run all the methods that don't require tuning here
-# exp.methods =  [
-#                 'majority',
-#                 # 'mace',
-#                 # 'ds',
-#                 # #'best',
-#                 # #'worst',
-#
-# ]
-#
-# # this will run task 1 -- train on all crowdsourced data, test on the labelled portion thereof
-# exp.run_methods(annos, gt, doc_start, output_dir, text,
-#                 ground_truth_val=gt_dev, doc_start_val=doc_start_dev, text_val=text_dev,
-#                 new_data=regen_data
-#                 )
-#
+exp = Experiment(None, 3, annos.shape[1], None, max_iter=20)
+
+exp.save_results = True
+exp.opt_hyper = False
+
+exp.alpha0_diags = best_diags
+exp.alpha0_factor = best_factor
+exp.nu0_factor = best_nu0factor
+
+# # run all the methods that don't require tuning here
+exp.methods =  [
+                'bac_seq_integrateIF'
+                # 'majority',
+                # 'mace',
+                # 'ds',
+                # #'best',
+                # #'worst',
+
+]
+
+# this will run task 1 -- train on all crowdsourced data, test on the labelled portion thereof
+exp.run_methods(annos, gt, doc_start, output_dir, text,
+                ground_truth_val=gt_dev, doc_start_val=doc_start_dev, text_val=text_dev,
+                new_data=regen_data
+                )
+
 #
 # #  ------------------------------------------------------------------------------------------------
 # exp = Experiment(None, 3, annos.shape[1], None, max_iter=20)
