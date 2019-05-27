bd364af4f103096395b819a5524101a34e7f5f0e
Edwin Simpson <simpson@ukp.informatik.tu-darmstadt.de>
2019-05-27 16:43:53 +0200

    submitted version for EMNLP

--- src/significance.py
+++ src/significance.py
@@ -3,13 +3,13 @@ from scipy.stats import wilcoxon
 
 outroot = os.path.expanduser('~/data/bayesian_sequence_combination/output/')
 
-baseline = outroot + '/ner/pred_started-2019-05-17-09-52-22-Nseen29704.csv'
+baseline = outroot + 'ner/pred_started-2019-05-17-09-52-22-Nseen29704.csv'
 
-bscseq = outroot + '/ner/pred_started-2019-04-11-10-24-38-Nseen29704.csv'
+bscseq = outroot + 'ner/pred_started-2019-04-11-10-24-38-Nseen29704.csv'
 
-preds_base = pd.read_csv(baseline)
+preds_base = pd.read_csv(baseline).values.flatten()
 
-preds_bscseq = pd.read_csv(bscseq)
+preds_bscseq = pd.read_csv(bscseq).values.flatten()
 
 _, p = wilcoxon(preds_base, preds_bscseq)
 
@@ -19,11 +19,11 @@ print('NER: BSC-seq %s significantly different with p = %f' % ('is' if p<0.05 el
 
 baseline = outroot + '/pico2/pred_started-2019-05-11-14-24-25-Nseen56858.csv'
 
-bscseq = outroot + '/bio_task1/pred_started-2018-08-25-18-08-57-Nseen56858.csv'
+bscseq = outroot + '/pico2/pred_started-2019-05-17-11-16-12-Nseen56858.csv'
 
-preds_base = pd.read_csv(baseline)
+preds_base = pd.read_csv(baseline).values.flatten()
 
-preds_bscseq = pd.read_csv(bscseq)
+preds_bscseq = pd.read_csv(bscseq).values.flatten()
 
 _, p = wilcoxon(preds_base, preds_bscseq)
 
@@ -35,9 +35,9 @@ baseline = outroot + '/arg_LMU_final/pred_started-2019-05-10-18-50-04-Nseen40000
 
 bscseq = outroot + '/arg_LMU_final/pred_started-2019-05-09-23-33-51-Nseen40000.csv'
 
-preds_base = pd.read_csv(baseline)
+preds_base = pd.read_csv(baseline).values.flatten()
 
-preds_bscseq = pd.read_csv(bscseq)
+preds_bscseq = pd.read_csv(bscseq).values.flatten()
 
 _, p = wilcoxon(preds_base, preds_bscseq)
 
