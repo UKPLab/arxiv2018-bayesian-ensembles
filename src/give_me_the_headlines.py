2451ecf9189590866f3119e4c819d79ca42d4570
Edwin Simpson <simpson@ukp.informatik.tu-darmstadt.de>
2019-09-04 00:27:23 +0200

    include means

--- src/give_me_the_headlines.py
+++ src/give_me_the_headlines.py
@@ -13,6 +13,9 @@ if len(sys.argv) > 2:
 else:
     base_models = ['bilstm-crf', 'crf'] # , 'flair-pos', 'flair-ner']
 
+totals_tok = {}
+totals_rel = {}
+
 for classid in range(4):
     basemodels_str = '--'.join(base_models)
 
@@ -25,5 +28,18 @@ for classid in range(4):
         res = json.load(fh)
 
     for key in res:
+        f1tok = 100*np.mean(res[key], 0)[1]
+        f1rel = 100*np.mean(res[key], 0)[3]
+
         print('Spantype %i: token f1 = %.1f; relaxed span f1 = %.1f, %s'
-          % (classid, 100*np.mean(res[key], 0)[1], 100*np.mean(res[key], 0)[3], key))
+          % (classid, f1tok, f1rel, key))
+        if key not in totals_tok:
+            totals_tok[key] = f1tok
+            totals_rel[key] = f1rel
+        else:
+            totals_tok[key] += f1tok
+            totals_rel[key] += f1rel
+
+for key in totals_tok:
+    print('Mean: token f1 = %.1f; relaxed span f1 = %.1f, %s' %
+          (totals_tok[key] / 4.0, totals_rel[key] / 4.0, key))
