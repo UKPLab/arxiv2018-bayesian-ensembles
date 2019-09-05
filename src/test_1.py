363fa08bfc85aef655bd50e66d3be95537efb253
Edwin Simpson <simpson@ukp.informatik.tu-darmstadt.de>
2019-09-06 01:00:33 +0200

    reduce epochs

--- src/test_1.py
+++ src/test_1.py
@@ -70,7 +70,7 @@ datadir = os.path.join(get_root_dir(), 'data/famulus_%s' % uberdomain)
 
 nclasses = 9 # four types means I and B tags for each type + 1 O tag gives 9 different tags or class labels
 
-base_models = ['crf'] # 'bilstm-crf']#'crf',  'flair-pos']#, 'flair-ner'] # 'flair' -- not implemented yet, do this if we can't get lample to work
+base_models = ['bilstm-crf', 'crf']#'crf',  'flair-pos']#, 'flair-ner'] # 'flair' -- not implemented yet, do this if we can't get lample to work
 
 
 print('Using base models: ' + str(base_models))
