0877b1620a2b4b1d73c2d65f83d7251c312559cb
Edwin Simpson <simpson@wormulon.ukp.informatik.tu-darmstadt.de>
2019-11-19 16:56:46 +0100

    merge some more changes from emnlp

---
+++ batch_scripts/run_pico_wormulon.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -J pico
+#SBATCH --mail-user=simpson@ukp.informatik.tu-darmstadt.de
+#SBATCH --mail-type=FAIL
+#SBATCH --partition=ukp
+#SBATCH --output=/ukp-storage-1/simpson/pico.txt
+#SBATCH -e ./pico.err.%j
+#SBATCH -o ./pico.out.%j
+#SBATCH -n 1
+#SBATCH -c 16
+#SBATCH --mem-per-cpu=8182
+
+
+# ----------------------------------
+
+
+# module load intel python/3.6.8
+source /ukp-storage-1/simpson/git/bayesian_annotator_combination/env/bin/activate
+python -u /ukp-storage-1/simpson/git/bayesian_annotator_combination/src/run_pico_experiments.py
