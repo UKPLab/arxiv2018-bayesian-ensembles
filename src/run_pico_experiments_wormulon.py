d9e3b6a40a33a82debe5c052ed50415cfabbb2c9
Edwin Simpson <simpson@ukp.informatik.tu-darmstadt.de>
2019-11-21 14:56:39 +0100

    wormulon testing again

--- src/run_pico_experiments_wormulon.py
+++ src/run_pico_experiments_wormulon.py
@@ -9,7 +9,7 @@ from evaluation.experiment import Experiment
 import data.load_data as load_data
 import numpy as np
 
-output_dir = os.path.join(load_data.output_root_dir, 'pico_no_outside_factor_weighting')
+output_dir = os.path.join(load_data.output_root_dir, 'pico_wormulon')
 regen_data = False
 
 gt, annos, doc_start, text, gt_task1_dev, gt_dev, doc_start_dev, text_dev = \
@@ -34,16 +34,33 @@ exp.nu0_factor = best_nu0factor
 # # run all the methods that don't require tuning here
 exp.methods =  [
                 'bac_seq_integrateIF',
-                # 'majority',
-                # 'mace',
-                # 'ds',
-                # 'ibcc',
-                # 'bac_vec_integrateIF',
-                # 'bac_ibcc_integrateIF',
-                # 'bac_mace_integrateIF',
-                # 'HMM_crowd',
-                # 'best',
-                # 'worst',
+]
+
+# this will run task 1 -- train on all crowdsourced data, test on the labelled portion thereof
+exp.run_methods(annos, gt, doc_start, output_dir, text,
+                 ground_truth_val=gt_dev, doc_start_val=doc_start_dev, text_val=text_dev,
+                 new_data=regen_data
+                 )
+
+# ------------------------------------------------------------------------------------------------
+# this is the one we used in the paper, result_started-2019-08-22-06-17-54-Nseen56858.csv
+best_nu0factor = 0.01
+best_diags = 0.1
+best_factor = 1
+best_outside_factor = 5
+
+exp = Experiment(None, 3, annos.shape[1], None, max_iter=20, outside_factor=best_outside_factor)
+
+exp.save_results = True
+exp.opt_hyper = False
+
+exp.alpha0_diags = best_diags
+exp.alpha0_factor = best_factor
+exp.nu0_factor = best_nu0factor
+
+# # run all the methods that don't require tuning here
+exp.methods =  [
+                'bac_seq_integrateIF',
 ]
 
 # this will run task 1 -- train on all crowdsourced data, test on the labelled portion thereof
@@ -58,19 +75,7 @@ diags = [0.1, 1, 10]
 factors = [0.1, 1, 10]
 
 methods_to_tune = [
-                # 'ibcc',
-                'bac_seq_integrateIF',
-                # 'bac_vec_integrateIF',
-                # 'bac_ibcc_integrateIF',
-                # 'HMM_crowd',
-                # 'bac_acc_integrateIF',
-                # 'bac_mace_integrateIF',
-                # # 'bac_ibcc',
-                # # 'bac_ibcc_integrateIF_noHMM',
-                # # 'bac_seq',
-                # # 'bac_seq_integrateIF_noHMM',
-                # 'mace',
-                # 'bac_mace_noHMM'
+                   'bac_seq_integrateIF',
                    ]
 
 best_bac_wm_score = -np.inf
@@ -105,13 +110,13 @@ tune_text = text[idxs]
 #    )
 
 # # run all the methods that don't require tuning here
-exp.methods =  [
-                'bac_seq_integrateIF_then_LSTM',
-                'bac_seq_integrateIF_integrateLSTM_atEnd',
-]
-
-# this will run task 1 -- train on all crowdsourced data, test on the labelled portion thereof
-exp.run_methods(annos, gt, doc_start, output_dir, text,
-                ground_truth_val=gt_dev, doc_start_val=doc_start_dev, text_val=text_dev,
-                new_data=regen_data
-                )
+# exp.methods =  [
+#                 'bac_seq_integrateIF_then_LSTM',
+#                 'bac_seq_integrateIF_integrateLSTM_atEnd',
+# ]
+#
+# # this will run task 1 -- train on all crowdsourced data, test on the labelled portion thereof
+# exp.run_methods(annos, gt, doc_start, output_dir, text,
+#                 ground_truth_val=gt_dev, doc_start_val=doc_start_dev, text_val=text_dev,
+#                 new_data=regen_data
+#                 )
