eba143ccf4131d57ca9f2a9888c1bda9e06df9ed
Edwin Simpson <simpson@ukp.informatik.tu-darmstadt.de>
2019-12-17 09:36:18 +0100

    trying the new settings on the other splits

--- src/run_pico_experiments_wormulon.py
+++ src/run_pico_experiments_wormulon.py
@@ -21,6 +21,8 @@ import numpy as np
 # So up until then, it may have been using 160818 or 1705118.
 
 # Longer-term question -- why does it make so much difference to BSC but not to HMM-crowd? What is it sensitive to?
+# Since performance on Lichtenberg is also a bit different, is there some kind of machine precision error that we come
+# up against? Or perhaps HMMcrowd hits the precision error earlier?
 regen_data = False
 
 datadir = 'bio_160818maybe'
@@ -31,7 +33,7 @@ gt, annos, doc_start, text, gt_task1_dev, gt_dev, doc_start_dev, text_dev = \
 best_nu0factor = 1
 best_diags = 10
 best_factor = 100
-best_outside_factor = 1
+best_outside_factor = 5
 
 # ------------------------------------------------------------------------------------------------
 exp = Experiment(None, 3, annos.shape[1], None, max_iter=20, outside_factor=best_outside_factor)
@@ -48,8 +50,8 @@ exp.methods =  [
                 'bac_seq_integrateIF',
 ]
 
-output_dir = os.path.join(load_data.output_root_dir, 'pico_wormulon_%f_%f_%f_%s'
-                          % (best_nu0factor, best_diags, best_factor, datadir))
+output_dir = os.path.join(load_data.output_root_dir, 'pico_wormulon_%f_%f_%f_%f_%s'
+                          % (best_nu0factor, best_diags, best_factor, best_outside_factor, datadir))
 
 # this will run task 1 -- train on all crowdsourced data, test on the labelled portion thereof
 exp.run_methods(annos, gt, doc_start, output_dir, text,
@@ -66,7 +68,7 @@ gt, annos, doc_start, text, gt_task1_dev, gt_dev, doc_start_dev, text_dev = \
 best_nu0factor = 1
 best_diags = 10
 best_factor = 100
-best_outside_factor = 1
+best_outside_factor = 5
 
 # ------------------------------------------------------------------------------------------------
 exp = Experiment(None, 3, annos.shape[1], None, max_iter=20, outside_factor=best_outside_factor)
@@ -83,8 +85,8 @@ exp.methods =  [
                 'bac_seq_integrateIF',
 ]
 
-output_dir = os.path.join(load_data.output_root_dir, 'pico_wormulon_%f_%f_%f_%s'
-                          % (best_nu0factor, best_diags, best_factor, datadir))
+output_dir = os.path.join(load_data.output_root_dir, 'pico_wormulon_%f_%f_%f_%f_%s'
+                          % (best_nu0factor, best_diags, best_factor, best_outside_factor, datadir))
 
 # this will run task 1 -- train on all crowdsourced data, test on the labelled portion thereof
 exp.run_methods(annos, gt, doc_start, output_dir, text,
@@ -96,35 +98,35 @@ datadir = 'bio_160818maybe'
 gt, annos, doc_start, text, gt_task1_dev, gt_dev, doc_start_dev, text_dev = \
     load_data.load_biomedical_data(regen_data, data_folder=datadir)#, debug_subset_size=50000)
 
-# ------------------------------------------------------------------------------------------------
-# this is the one we used in the paper, result_started-2019-08-22-06-17-54-Nseen56858.csv
-best_nu0factor = 0.1
-best_diags = 0
-best_factor = 0.1
-best_outside_factor = 1
-
-exp = Experiment(None, 3, annos.shape[1], None, max_iter=20, outside_factor=best_outside_factor)
-
-exp.save_results = True
-exp.opt_hyper = False
-
-exp.alpha0_diags = best_diags
-exp.alpha0_factor = best_factor
-exp.nu0_factor = best_nu0factor
-
-# # run all the methods that don't require tuning here
-exp.methods =  [
-                'HMM_crowd',
-]
-
-output_dir = os.path.join(load_data.output_root_dir, 'pico_wormulon_%f_%f_%f_%s'
-                          % (best_nu0factor, best_diags, best_factor, datadir))
-
-# this will run task 1 -- train on all crowdsourced data, test on the labelled portion thereof
-exp.run_methods(annos, gt, doc_start, output_dir, text,
-                 ground_truth_val=gt_dev, doc_start_val=doc_start_dev, text_val=text_dev,
-                 new_data=regen_data
-                 )
+# # ------------------------------------------------------------------------------------------------
+# # this is the one we used in the paper, result_started-2019-08-22-06-17-54-Nseen56858.csv
+# best_nu0factor = 0.1
+# best_diags = 0
+# best_factor = 0.1
+# best_outside_factor = 1
+#
+# exp = Experiment(None, 3, annos.shape[1], None, max_iter=20, outside_factor=best_outside_factor)
+#
+# exp.save_results = True
+# exp.opt_hyper = False
+#
+# exp.alpha0_diags = best_diags
+# exp.alpha0_factor = best_factor
+# exp.nu0_factor = best_nu0factor
+#
+# # # run all the methods that don't require tuning here
+# exp.methods =  [
+#                 'HMM_crowd',
+# ]
+#
+# output_dir = os.path.join(load_data.output_root_dir, 'pico_wormulon_%f_%f_%f_%s'
+#                           % (best_nu0factor, best_diags, best_factor, datadir))
+#
+# # this will run task 1 -- train on all crowdsourced data, test on the labelled portion thereof
+# exp.run_methods(annos, gt, doc_start, output_dir, text,
+#                  ground_truth_val=gt_dev, doc_start_val=doc_start_dev, text_val=text_dev,
+#                  new_data=regen_data
+#                  )
 #
 # # ------------------------------------------------------------------------------------------------
 # nu_factors = [0.01, 0.1, 1]
