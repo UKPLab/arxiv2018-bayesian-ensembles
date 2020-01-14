3c0db5884bac9a4028b9e934972fd871a6b9d7f1
Edwin Simpson <simpson@ukp.informatik.tu-darmstadt.de>
2020-01-14 13:51:48 +0100

    OMP_NUM_THREADS options

--- batch_scripts/run_ner_wormulon.sh
+++ batch_scripts/run_ner_wormulon.sh
@@ -14,4 +14,4 @@
 
 # module load intel python/3.6.8
 source /ukp-storage-1/simpson/git/bayesian_annotator_combination/env/bin/activate
-python OMP_NUM_THREADS=8 -u /ukp-storage-1/simpson/git/bayesian_annotator_combination/src/run_ner_experiments_gpu.py
+OMP_NUM_THREADS=8 python -u /ukp-storage-1/simpson/git/bayesian_annotator_combination/src/run_ner_experiments_gpu.py
--- batch_scripts/run_pico_wormulon.sh
+++ batch_scripts/run_pico_wormulon.sh
@@ -16,4 +16,4 @@
 
 # module load intel python/3.6.8
 source /ukp-storage-1/simpson/git/bayesian_annotator_combination/env/bin/activate
-python OMP_NUM_THREADS=8 -u /ukp-storage-1/simpson/git/bayesian_annotator_combination/src/run_pico_experiments_wormulon.py
+OMP_NUM_THREADS=8 python -u /ukp-storage-1/simpson/git/bayesian_annotator_combination/src/run_pico_experiments_wormulon.py
