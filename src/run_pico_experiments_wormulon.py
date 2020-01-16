61126c4b3103649d8c70b72eb6551ba4ec1e0eaa
Edwin Simpson <simpson@ukp.informatik.tu-darmstadt.de>
2020-01-16 14:09:13 +0100

    pico expt last run

--- src/run_pico_experiments_wormulon.py
+++ src/run_pico_experiments_wormulon.py
@@ -170,7 +170,7 @@ gt, annos, doc_start, text, gt_task1_dev, gt_dev, doc_start_dev, text_dev = \
 
 # THE BEST SO FAR, but with different outside factor
 best_nu0factor = 1
-best_diags = 10
+best_diags = 100
 best_factor = 10
 best_begin_factor = 1
 
@@ -197,64 +197,64 @@ exp.run_methods(annos, gt, doc_start, output_dir, text,
                  ground_truth_val=gt_dev, doc_start_val=doc_start_dev, text_val=text_dev,
                  new_data=regen_data
                  )
-# THE BEST SO FAR
-best_nu0factor = 1
-best_diags = 10
-best_factor = 10
-best_begin_factor = 2
-
-exp = Experiment(None, 3, annos.shape[1], None, max_iter=20, begin_factor=best_begin_factor)
-
-exp.save_results = True
-exp.opt_hyper = False
-
-exp.alpha0_diags = best_diags
-exp.alpha0_factor = best_factor
-exp.nu0_factor = best_nu0factor
-
-# # run all the methods that don't require tuning here
-exp.methods =  [
-                'ibcc',
-                'bac_seq_integrateIF',
-]
-
-output_dir = os.path.join(load_data.output_root_dir, 'pico_wormulon_%f_%f_%f_%f_%s_betaOO_nu0_1_prior'
-                          % (best_nu0factor, best_diags, best_factor, best_begin_factor, datadir))
-
-# this will run task 1 -- train on all crowdsourced data, test on the labelled portion thereof
-exp.run_methods(annos, gt, doc_start, output_dir, text,
-                 ground_truth_val=gt_dev, doc_start_val=doc_start_dev, text_val=text_dev,
-                 new_data=regen_data
-                 )
-# THE BEST SO FAR
-best_nu0factor = 1
-best_diags = 10
-best_factor = 10
-best_begin_factor = 5
-
-exp = Experiment(None, 3, annos.shape[1], None, max_iter=20, begin_factor=best_begin_factor)
-
-exp.save_results = True
-exp.opt_hyper = False
-
-exp.alpha0_diags = best_diags
-exp.alpha0_factor = best_factor
-exp.nu0_factor = best_nu0factor
-
-# # run all the methods that don't require tuning here
-exp.methods =  [
-                'ibcc',
-                'bac_seq_integrateIF',
-]
-
-output_dir = os.path.join(load_data.output_root_dir, 'pico_wormulon_%f_%f_%f_%f_%s_betaOO_nu0_1_prior'
-                          % (best_nu0factor, best_diags, best_factor, best_begin_factor, datadir))
-
-# this will run task 1 -- train on all crowdsourced data, test on the labelled portion thereof
-exp.run_methods(annos, gt, doc_start, output_dir, text,
-                 ground_truth_val=gt_dev, doc_start_val=doc_start_dev, text_val=text_dev,
-                 new_data=regen_data
-                 )
+# # THE BEST SO FAR
+# best_nu0factor = 1
+# best_diags = 10
+# best_factor = 10
+# best_begin_factor = 2
+#
+# exp = Experiment(None, 3, annos.shape[1], None, max_iter=20, begin_factor=best_begin_factor)
+#
+# exp.save_results = True
+# exp.opt_hyper = False
+#
+# exp.alpha0_diags = best_diags
+# exp.alpha0_factor = best_factor
+# exp.nu0_factor = best_nu0factor
+#
+# # # run all the methods that don't require tuning here
+# exp.methods =  [
+#                 'ibcc',
+#                 'bac_seq_integrateIF',
+# ]
+#
+# output_dir = os.path.join(load_data.output_root_dir, 'pico_wormulon_%f_%f_%f_%f_%s_betaOO_nu0_1_prior'
+#                           % (best_nu0factor, best_diags, best_factor, best_begin_factor, datadir))
+#
+# # this will run task 1 -- train on all crowdsourced data, test on the labelled portion thereof
+# exp.run_methods(annos, gt, doc_start, output_dir, text,
+#                  ground_truth_val=gt_dev, doc_start_val=doc_start_dev, text_val=text_dev,
+#                  new_data=regen_data
+#                  )
+# # THE BEST SO FAR
+# best_nu0factor = 1
+# best_diags = 10
+# best_factor = 10
+# best_begin_factor = 5
+#
+# exp = Experiment(None, 3, annos.shape[1], None, max_iter=20, begin_factor=best_begin_factor)
+#
+# exp.save_results = True
+# exp.opt_hyper = False
+#
+# exp.alpha0_diags = best_diags
+# exp.alpha0_factor = best_factor
+# exp.nu0_factor = best_nu0factor
+#
+# # # run all the methods that don't require tuning here
+# exp.methods =  [
+#                 'ibcc',
+#                 'bac_seq_integrateIF',
+# ]
+#
+# output_dir = os.path.join(load_data.output_root_dir, 'pico_wormulon_%f_%f_%f_%f_%s_betaOO_nu0_1_prior'
+#                           % (best_nu0factor, best_diags, best_factor, best_begin_factor, datadir))
+#
+# # this will run task 1 -- train on all crowdsourced data, test on the labelled portion thereof
+# exp.run_methods(annos, gt, doc_start, output_dir, text,
+#                  ground_truth_val=gt_dev, doc_start_val=doc_start_dev, text_val=text_dev,
+#                  new_data=regen_data
+#                  )
 
 # # from early paper draft
 # best_nu0factor = 100
