6db11c519fe44aa7ff02a1c322f4f43b7b60e05a
Edwin Simpson <edwin.simpson@gmail.com>
2020-08-12 20:19:24 +0200

    fix script

--- batch_scripts/PBSPro/run_NER_EMNLP19.sh
+++ batch_scripts/PBSPro/run_NER_EMNLP19.sh
@@ -11,7 +11,7 @@
 
 # request resources and set limits
 #PBS -l walltime=72:00:00
-#PBS -l select=1:ncpus=28:mem=16GB
+#PBS -l select=1:ncpus=24:mem=16GB
 #:ompthreads=24
 # 'select' chooses number of nodes.
 
@@ -19,10 +19,10 @@
 module load lang/python/anaconda/pytorch lang/cuda
 
 # We might need to add the global paths to our code to the pythonpath. Also set the data directories globally.
-cd /work/es1595/arxiv2018-bayesian-ensembles
+cd /work/es1595/arxiv2018-bayesian-ensembles/src
 
 #  run the script
-python -u src/experiments/EMNLP2019/run_ner_experiments.py
+python -u experiments/EMNLP2019/run_ner_experiments.py
 
 # To submit: qsub run_NER_EMNLP19.sh
 # To display the queue: qstat -Q gpu (this is usually where the GPU job ends up)
